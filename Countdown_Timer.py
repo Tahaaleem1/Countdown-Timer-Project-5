a2f90be41b1a3d37a029a93ebcc01f630b7bdf40
Taha <156198811+Tahaaleem1@users.noreply.github.com>
2025-04-27 23:29:10 +0500

    Update Countdown_Timer.py

--- Countdown_Timer.py
+++ Countdown_Timer.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyOZHD9tcQAU3asDvO8gMp5a",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
